107b52e4039f65acde4caffdbbf3ed285308e4a4
sindhu <schowdary450@gmail.com>
2025-05-24 15:02:16 -0400

    webdriver changes

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -7,25 +7,20 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import io.github.bonigarcia.wdm.WebDriverManager; // Add this import
 
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData()
+    {
+        System.out.println("Hello Guys");
+        WebDriverManager.chromedriver().setup(); // Use WebDriverManager to set up ChromeDriver
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+        String text = driver.findElement(By.cssSelector("h1")).getText();
+        System.out.println(text);
+        Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+        driver.close();
+    }
 }
